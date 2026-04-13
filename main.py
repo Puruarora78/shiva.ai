93f296589f4a5dd16f80f3825d53c6afad67a76a
Puru Arora <puruarora28@gmail.com>
2026-04-13 20:41:30 +0530

    added book

---
+++ main.py
@@ -0,0 +1,8 @@
+import pyttsx3
+import speech_recognition as spe
+import webbrowser as web
+
+shiva = pyttsx3.init()
+
+if __name__ == "__main__":
+    pyttsx3.speak("Hello, I am Shiva. How can I help you today?")
